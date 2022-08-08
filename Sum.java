5f10ed02ac94032a07b0dbdd4b0af66619bdc827
Aditya <aditya.desity20@pccoepune.org>
2022-08-08 23:20:20 +0530

    intial commit

---
+++ Sum.java
@@ -0,0 +1,10 @@
+public class Sum {
+    static int sum(int a,int b){
+        return a+b;
+    }
+    public static void main(String []args){
+        int a=10;
+        int b=20;
+        System.out.println(sum(a,b));
+    }
+}
