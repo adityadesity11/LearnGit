617ebac1ac38f15a42859ca4857deb339095c803
Aditya <aditya.desity20@pccoepune.org>
2022-08-08 23:38:43 +0530

    added Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,10 @@
+public class Multiply {
+    static int mpy(int a,int b){
+        return a*b;
+    }
+    public static void main(String []args){
+        int a = 10;
+        int b = 20;
+        System.out.println(mpy(a,b));
+    }
+}
--- Sum.java
+++ Sum.java
@@ -2,6 +2,7 @@ public class Sum {
     static int sum(int a,int b){
         return a+b;
     }
+
     public static void main(String []args){
         int a=10;
         int b=20;
