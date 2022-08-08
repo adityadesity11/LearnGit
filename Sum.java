9b0cc8ced6d5533d30eea45d5f210ed5a319253d
Aditya <aditya.desity20@pccoepune.org>
2022-08-08 23:24:52 +0530

    Modifying Sum.java

--- Sum.java
+++ Sum.java
@@ -5,6 +5,6 @@ public class Sum {
     public static void main(String []args){
         int a=10;
         int b=20;
-        System.out.println(sum(a,b));
+        System.out.println("The sum is: "+sum(a,b));
     }
 }
